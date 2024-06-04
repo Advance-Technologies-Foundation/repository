99f8fec1436bc86b53633eaa36bd46dd7a5b45f3
karterfree <karterfree@gmail.com>
2024-06-04 23:28:41 +0300

    Update ATF.Repository to support NTLM authentication. Additional

--- ATF.Repository/Frienly.cs
+++ ATF.Repository/Frienly.cs
@@ -3,3 +3,4 @@
 [assembly: InternalsVisibleTo("ATF.Repository.Tests")]
 [assembly: InternalsVisibleTo("ATF.Repository.Core.UnitTests")]
 [assembly: InternalsVisibleTo("ATF.Repository.FW.UnitTests")]
+[assembly: InternalsVisibleTo("ATF.Repository.Mock")]
